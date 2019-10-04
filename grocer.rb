2650a702a162bca6d6781f0482fe104f42e0a5f1
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-04 16:27:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,14 @@
-def consolidate_cart(cart)
-  # code here
-end
-
-def apply_coupons(cart, coupons)
-  # code here
-end
-
-def apply_clearance(cart)
-  # code here
-end
-
-def checkout(cart, coupons)
-  # code here
+def consolidate_cart(cart:[])
+  # code here	  # code here
+  con_hash = {}
+  cart.each do |item|
+    item.each do |name, attribute|
+      if con_hash.has_key?(name)
+        con_hash[name][:count] += 1        
+      else 
+        con_hash = con_hash.merge({name => attribute.merge({count: 1})})
+      end
+    end
+  end
+  return con_hash
 end
